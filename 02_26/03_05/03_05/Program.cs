fd146d59c1256405c47a6a56181d9dcd31ac9740
EduardoFertigBastos <dudufbastos1@gmail.com>
2021-03-05 20:49:31 -0300

    Class 03_05

---
+++ 02_26/03_05/03_05/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _03_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exercicio 1 
+            int[] umArray = new int[5];
+            umArray[0] = 1;
+            umArray[1] = 2;
+            umArray[2] = 3;
+            umArray[3] = 4;
+            umArray[4] = 5;
+
+            for (int i = 0; i < umArray.Length; i++)
+            {
+                Console.Write(umArray[i] + " ");
+            }*/
+
+
+            /* Exercicio 2  
+            string[] doisArray = new string[5];
+            doisArray[0] = "Cliente 1 email: cliente1@yahoo.com.br";
+            doisArray[1] = "Cliente 2 email: cliente2@yahoo.com.br";
+            doisArray[2] = "Cliente 3 email: cliente3@yahoo.com.br";
+            doisArray[3] = "Cliente 4 email: cliente4@yahoo.com.br";
+            doisArray[4] = "Cliente 5 email: cliente5@yahoo.com.br";
+
+            for (int i = 0; i < doisArray.Length; i++)
+            {
+                Console.Write(doisArray[i] + " ");
+            }
+            */
+
+            /* Exercicio 3
+            int[,] array2 = {
+                                {
+                                    1,
+                                    2
+                                }, 
+                                {
+                                    3,
+                                    4
+                                }, 
+                                {   
+                                    5, 
+                                    6
+                                }
+                            };
+
+            for (int i = 0; i < array2.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    Console.WriteLine(array2[i, j]);
+                }
+            }*/
+        }
+    }
+}
